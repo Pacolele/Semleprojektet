346c165440f92e0414191b343d2751c7cac081ba
Elliot Hernesten <elliot@personalkollen.se>
2025-03-27 14:26:20 +0100

    Added static import in DEBUG mode

--- semlesite/settings.py
+++ semlesite/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIR = [
+        BASE_DIR / 'static'
+        ]
+
+STATIC_ROOT = BASE_DIR.parent  / 'semlesite' / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
